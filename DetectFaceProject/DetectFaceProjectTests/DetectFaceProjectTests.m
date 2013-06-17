b915d70e6d5119e31ac46f76a57cdeee81835c12
vominhthong <santalucia1992@gmail.com>
2013-06-17 20:00:10 +0700

    #1 Setup Project This is version 0.1

---
+++ DetectFaceProject/DetectFaceProjectTests/DetectFaceProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  DetectFaceProjectTests.m
+//  DetectFaceProjectTests
+//
+//  Created by MC372 on 6/17/13.
+//  Copyright (c) 2013 MC372. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DetectFaceProjectTests : XCTestCase
+
+@end
+
+@implementation DetectFaceProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
